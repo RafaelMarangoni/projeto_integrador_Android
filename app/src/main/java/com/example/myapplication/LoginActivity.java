7a8f328ba99c9eeafb97dfe3d532e5bbcf1277c4
RafaelMarangoni <rafael.dmarangoni89@gmail.com>
2020-04-13 11:27:39 -0300

    alterações da branch

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button  buttonCadastro;
+
+    Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonCadastro = findViewById(R.id.cadastro);
+
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cadastroIntent = new Intent(activity, CadastroActivity.class);
+                startActivity(cadastroIntent);
+            }
+        });
+
     }
 }
