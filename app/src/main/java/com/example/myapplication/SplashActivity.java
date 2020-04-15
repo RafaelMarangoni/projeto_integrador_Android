9878aa7452d690e993875ed639b133fe0c566cdc
FelipeRapacci13 <61214250+FelipeRapacci13@users.noreply.github.com>
2020-04-15 19:31:54 -0300

    add tela splash

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Activity activity = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(activity, LoginActivity.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
