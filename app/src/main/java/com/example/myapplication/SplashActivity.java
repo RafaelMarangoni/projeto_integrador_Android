9bc4675568c2a5e7b42d6df727a87461042a8033
Daniel Wong Costa <48858864+danielwcosta@users.noreply.github.com>
2020-04-24 14:55:28 -0300

    Tela splash feita da forma correta

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -13,14 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(activity, LoginActivity.class));
-                finish();
-            }
-        }, 5000);
-
+        startActivity(new Intent(activity, LoginActivity.class));
+        finish();
     }
 }
