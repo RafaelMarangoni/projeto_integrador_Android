4e2852715f707eaf6f0665e56610872f2cbf5d24
Daniel Wong Costa <48858864+danielwcosta@users.noreply.github.com>
2020-04-24 03:48:11 -0300

    validação tela Cadastro, mais navegaçao entre telas:Cadastro-ListaFavoritos-ProcurarFavoritos ---- arrumado bug regras.xml

--- app/src/main/java/com/example/myapplication/ListaFavoritosActivity.java
+++ app/src/main/java/com/example/myapplication/ListaFavoritosActivity.java
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ListaFavoritosActivity extends AppCompatActivity {
 
+
+    private Button btnAddTime, btnRemoveTime;
+    private ImageView setaVoltar;
+
+    private Activity activity=this;
     private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,25 @@ public class ListaFavoritosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_favoritos);
 
         carregaFragment(new TimeFavoritoFragment());
+
+        btnAddTime = findViewById(R.id.lista_favoritos_add_time_id);
+        btnRemoveTime = findViewById(R.id.lista_favoritos_remover_time_id);
+        setaVoltar = findViewById(R.id.lista_favoritos_seta_voltar_id);
+
+        btnAddTime.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent procurarFavoritoIntent = new Intent(activity,ProcurarFavoritosActivity.class);
+                startActivity(procurarFavoritoIntent);
+            }
+        }));
+        setaVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(activity,MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 
     public void carregaFragment(Fragment fragment){
